7a7ea667fe875af6cb273a6e836ddeb399b36cc7
Lim Yeen Fei <yeenfei@neptrix.com>
2021-04-18 21:00:25 +0800

    -increment version for new release

--- Android/app/src/main/java/com/chipsley/whatsappsticker/StickerPackLoader.java
+++ Android/app/src/main/java/com/chipsley/whatsappsticker/StickerPackLoader.java
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.example.samplestickerapp;
+package com.chipsley.whatsappsticker;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -24,21 +24,21 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
-import static com.example.samplestickerapp.StickerContentProvider.ANDROID_APP_DOWNLOAD_LINK_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.ANIMATED_STICKER_PACK;
-import static com.example.samplestickerapp.StickerContentProvider.AVOID_CACHE;
-import static com.example.samplestickerapp.StickerContentProvider.IMAGE_DATA_VERSION;
-import static com.example.samplestickerapp.StickerContentProvider.IOS_APP_DOWNLOAD_LINK_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.LICENSE_AGREENMENT_WEBSITE;
-import static com.example.samplestickerapp.StickerContentProvider.PRIVACY_POLICY_WEBSITE;
-import static com.example.samplestickerapp.StickerContentProvider.PUBLISHER_EMAIL;
-import static com.example.samplestickerapp.StickerContentProvider.PUBLISHER_WEBSITE;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_FILE_EMOJI_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_FILE_NAME_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_ICON_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_IDENTIFIER_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_NAME_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_PUBLISHER_IN_QUERY;
+import static com.chipsley.whatsappsticker.StickerContentProvider.ANDROID_APP_DOWNLOAD_LINK_IN_QUERY;
+import static com.chipsley.whatsappsticker.StickerContentProvider.ANIMATED_STICKER_PACK;
+import static com.chipsley.whatsappsticker.StickerContentProvider.AVOID_CACHE;
+import static com.chipsley.whatsappsticker.StickerContentProvider.IMAGE_DATA_VERSION;
+import static com.chipsley.whatsappsticker.StickerContentProvider.IOS_APP_DOWNLOAD_LINK_IN_QUERY;
+import static com.chipsley.whatsappsticker.StickerContentProvider.LICENSE_AGREENMENT_WEBSITE;
+import static com.chipsley.whatsappsticker.StickerContentProvider.PRIVACY_POLICY_WEBSITE;
+import static com.chipsley.whatsappsticker.StickerContentProvider.PUBLISHER_EMAIL;
+import static com.chipsley.whatsappsticker.StickerContentProvider.PUBLISHER_WEBSITE;
+import static com.chipsley.whatsappsticker.StickerContentProvider.STICKER_FILE_EMOJI_IN_QUERY;
+import static com.chipsley.whatsappsticker.StickerContentProvider.STICKER_FILE_NAME_IN_QUERY;
+import static com.chipsley.whatsappsticker.StickerContentProvider.STICKER_PACK_ICON_IN_QUERY;
+import static com.chipsley.whatsappsticker.StickerContentProvider.STICKER_PACK_IDENTIFIER_IN_QUERY;
+import static com.chipsley.whatsappsticker.StickerContentProvider.STICKER_PACK_NAME_IN_QUERY;
+import static com.chipsley.whatsappsticker.StickerContentProvider.STICKER_PACK_PUBLISHER_IN_QUERY;
 
 class StickerPackLoader {
 
